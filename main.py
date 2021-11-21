f393b548d92f94c710509936ffbe03cf42bb8ab3
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2021-11-21 21:26:40 +0000

    Added main file

---
+++ main.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands, tasks
+from datetime import datetime
+from pytz import timezone
+import asyncio
+
+TOKEN = "NzQyMzUzMjAzNzA1ODA2OTE4.XzE4TA.OB7GJin_EaqXlU0Puq1ueyJk-rE"
+
+bot = commands.Bot(command_prefix="-", case_insensitive=True)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send('pong')
+
+@bot.event
+async def on_ready():
+    change_status.start()
+    print("Time Lord is in the tardis")
+    print("-------------")
+    return await bot.change_presence(status=discord.Status.online)
+
+@tasks.loop(seconds=60)
+async def change_status():
+    # PST, MST, CST, EST, CEST, MSK, CEST
+    format = "%H:%M"
+
+    nowutc = datetime.now(timezone('UTC'))
+    nowpt = nowutc.astimezone(timezone('America/Los_Angeles'))
+    nowmt = nowutc.astimezone(timezone('America/Denver'))
+    nowct = nowutc.astimezone(timezone('America/Chicago'))
+    nowet = nowutc.astimezone(timezone('America/New_York'))
+    nowcet = nowutc.astimezone(timezone('Europe/Paris'))
+    nowmsk = nowutc.astimezone(timezone('Europe/Oslo'))
+    nowcst = nowutc.astimezone(timezone('Asia/Shanghai'))
+
+    name = (f"{nowutc.strftime(format)} UTC | Time Lord")
+    await bot.edit(nick=name)
+
+    await bot.change_presence(activity=discord.Game(f"""{nowpt.strftime(format)} PT | {nowmt.strftime(format)} MT | {nowct.strftime(format)} CT | {nowet.strftime(format)} ET | {nowcet.strftime(format)} CET | {nowmsk.strftime(format)} MSK | {nowcst.strftime(format)} CST
+    """))
+
+
+@bot.command()
+async def reload(ctx):
+    change_status.cancel()
+    async with ctx.typing():
+        await asyncio.sleep(3)
+        change_status.start()
+    msg = await ctx.channel.send("Time Reloaded")
+
+
+bot.run(TOKEN)
+
